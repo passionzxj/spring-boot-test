5eb3622d9e07eb00ba280abf2eba05ac17d95213
zhangxinjian <zxj123456>
2021-01-29 14:42:22 +0800

    上传进度

--- jwt_security/src/main/java/com/zhang/JwtApplication.java
+++ jwt_security/src/main/java/com/zhang/JwtApplication.java
@@ -2,10 +2,26 @@ package com.zhang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class JwtApplication {
     public static void main(String[] args) {
         SpringApplication.run(JwtApplication.class, args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //允许上传的文件最大值
+        factory.setMaxFileSize(DataSize.of(2, DataUnit.GIGABYTES)); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.of(10, DataUnit.GIGABYTES));
+        return factory.createMultipartConfig();
     }
 }
